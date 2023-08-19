9031bb40b895ee04a5a0db5f59b432dc1a63fdc9
duduantero <xduduanterox@gmail.com>
2023-08-19 20:46:48 -0300

    update

---
+++ aulas/vetor_matriz.c
@@ -0,0 +1,27 @@
+#include  <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int v[10];
+    v[0] = 1;
+    printf("%d",*v);// * para retornar o valor que está na posição de memória
+    
+    //declaração de vetores com atrinuição
+    int* v1 = {5,11,3};
+    int v2[3];
+    v2[0]= 5;
+    v2[1]= 11;
+    v2[2]= 3;
+
+
+    for(int i = 0; i < sizeof(v1)/sizeof(int);i++){ 
+       while(v1[i < sizeof(v1[i]/sizeof(int))]){
+        if (v1[i] < ){
+
+        }
+       }
+        
+    }
+    return 0;
+}
