e242d81d9c2978fefeee7770f6526353b314f077
duduantero <xduduanterox@gmail.com>
2023-08-20 21:18:05 -0300

    lista1/exercicio1

--- lista1/atividade2/exercicio1.c
+++ lista1/atividade2/exercicio1.c
@@ -1,11 +1,33 @@
-#include  <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+int soma_array(int array[4]){
+    int total =0;
+    for(int i =0; i<4;i++){
+        total = total + array[i];
+    }
+    return total;
+}
+
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    setlocale(LC_ALL,"Portuguese");
+    int numero[4];
+    int j =0;
+    for (int i = 1; i < 5; i++){
+        numero[j] = atoi(argv[i]);
+        printf("%d - %d \n",i,numero[j]);
+    }
+
+    int resultado =soma_array(numero);
+    printf("A soma dos elementos do array: %d",resultado);
+
     return 0;
 }
+
+
 
 
 
