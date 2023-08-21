43df35df6e2360a266e244ae7a4136d4481251d0
duduantero <xduduanterox@gmail.com>
2023-08-20 22:09:42 -0300

    Update atividade2/exercicio2

--- lista1/atividade2/exercicio2.c
+++ lista1/atividade2/exercicio2.c
@@ -1,9 +1,34 @@
 #include  <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    setlocale(LC_ALL,"Portuguese");
+    int numero[4];
+    int total =0;
+    int menor,maior;
+
+    for (int i = 1; i < 5; i++){
+        numero[i-1] = atoi(argv[i]);
+         
+    }
+    maior = numero[0];
+    menor = numero[0]; 
+
+    for(int i = 0;i<4;i++){
+        printf("%d",numero[i]);
+        if(numero[i] < menor){
+            menor = numero[i];
+        }
+        if (numero[i] > maior)
+        {
+            maior = numero[i];
+        }   
+    }
+
+    printf("Menor numero: %d, menor numero: %d",menor,maior);
+
     return 0;
 }
 
