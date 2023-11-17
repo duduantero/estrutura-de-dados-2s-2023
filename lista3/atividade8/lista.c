0b83327b309dbb4ba697889ad33610e6655f5e1e
duduantero <xduduanterox@gmail.com>
2023-11-17 20:28:51 -0300

    update atividade8

--- lista3/atividade8/lista.c
+++ lista3/atividade8/lista.c
@@ -2,74 +2,158 @@
 #include <stdlib.h>
 #include "lista.h"
 
-No* no(char valor, No* proximo_no){
-    No* no = malloc(sizeof(No));
-    no->valor = valor;
-    no->proximo_no = proximo_no;
-    return no;
+No* no(char valor, No* proximo_no) {
+    No* novo_no = malloc(sizeof(No));
+    novo_no->valor = valor;
+    novo_no->proximo_no = proximo_no;
+    return novo_no;
 }
 
-void lista_inserir_no(No* L, No* no){
-    if(L != NULL){
-        if(L->proximo_no == NULL){
+void lista_inserir_no(No* L, No* no) {
+    if (L != NULL) {
+        if (L->proximo_no == NULL) {
             L->proximo_no = no;
-        }
-        else{
+        } else {
             lista_inserir_no(L->proximo_no, no);
         }
     }
 }
 
-void lista_imprimir(No* L){
-    if(L != NULL){
+void lista_imprimir(No* L) {
+    if (L != NULL) {
         printf("%c ", L->valor);
         lista_imprimir(L->proximo_no);
     }
 }
 
-int lista_quantidade_nos(No* L){
-    if(L != NULL){
+int lista_quantidade_nos(No* L) {
+    if (L != NULL) {
         return 1 + lista_quantidade_nos(L->proximo_no);
     }
     return 0;
 }
 
-No* lista_copiar(No* L){
-    if(L != NULL){
+No* lista_copiar(No* L) {
+    if (L != NULL) {
         return no(L->valor, lista_copiar(L->proximo_no));
     }
     return NULL;
 }
 
-void lista_concatenar(No* L, No* Lc){
-    lista_inserir_no(L, Lc);
+void lista_concatenar(No* L, No* Lc) {
+    if (L != NULL) {
+        while (L->proximo_no != NULL) {
+            L = L->proximo_no;
+        }
+        L->proximo_no = Lc;
+    }
 }
 
-void lista_liberar(No* L){
-    if(L != NULL){
+void lista_liberar(No* L) {
+    if (L != NULL) {
         lista_liberar(L->proximo_no);
         free(L);
     }
 }
 
-
-// Esta função recebe um inteiro x e uma
-// lista encadeada le de inteiros e devolve
-// o endereço de uma celula que contém x.
-// Se tal celula não existe, devolve NULL.
-int lista_verificar_existencia(No* L, char valor_busca){
-
-    valor_busca *p;
-    p = L;
-    while (p != NULL && p->valor != x) 
-      p = p->prox; 
-   return p;
-
+int lista_verificar_existencia(No* L, char valor_busca) {
+    No* p = L;
+    while (p != NULL && p->valor != valor_busca) 
+        p = p->proximo_no; 
+    return p != NULL;
 }
 
+int lista_verificar_ocorrencias(No* L, char valor_busca) {
+    int ocorrencias = 0;
+    while (L != NULL) {
+        if (L->valor == valor_busca) {
+            ocorrencias++;
+        }
+        L = L->proximo_no;
+    }
+    return ocorrencias;
+}
 
-int lista_verificar_ocorrencias(No* L, char valor_busca){}
-void lista_imprimir_inversa(No* L){}
-void lista_inserir_no_i(No* L, int i){}
-void lista_remover_no_i(No* L, int i){}
-void lista_remover_no(No* L, char valor_busca){}
+void lista_imprimir_inversa(No* L) {
+    if (L != NULL) {
+        lista_imprimir_inversa(L->proximo_no);
+        printf("%c ", L->valor);
+    }
+}
+
+void lista_inserir_no_i(No* L, int i) {
+    if (i < 0) {
+        printf("Índice inválido para inserção.\n");
+        return;
+    }
+
+    if (i == 0) {
+        printf("Insira o valor para o novo nó: ");
+        char novo_valor;
+        scanf(" %c", &novo_valor);
+        No* novo_no = no(novo_valor, L->proximo_no);
+        L->proximo_no = novo_no;
+        return;
+    }
+
+    int indice_atual = 0;
+    while (L != NULL && indice_atual < i - 1) {
+        L = L->proximo_no;
+        indice_atual++;
+    }
+
+    if (L == NULL) {
+        printf("Índice inválido para inserção.\n");
+        return;
+    }
+
+    printf("Insira o valor para o novo nó: ");
+    char novo_valor;
+    scanf(" %c", &novo_valor);
+    No* novo_no = no(novo_valor, L->proximo_no);
+    L->proximo_no = novo_no;
+}
+
+void lista_remover_no_i(No* L, int i) {
+    if (i < 0) {
+        printf("Índice inválido para remoção.\n");
+        return;
+    }
+
+    int indice_atual = 0;
+    while (L != NULL && indice_atual < i - 1) {
+        L = L->proximo_no;
+        indice_atual++;
+    }
+
+    if (L == NULL || L->proximo_no == NULL) {
+        printf("Índice inválido para remoção.\n");
+        return;
+    }
+
+    No* no_remover = L->proximo_no;
+    L->proximo_no = no_remover->proximo_no;
+    free(no_remover);
+}
+
+void lista_remover_no(No* L, char valor_busca) {
+    No* anterior = NULL;
+    No* atual = L;
+
+    while (atual != NULL) {
+        if (atual->valor == valor_busca) {
+            if (anterior == NULL) {
+                // Remoção do primeiro nó
+                L = atual->proximo_no;
+            } else {
+                anterior->proximo_no = atual->proximo_no;
+            }
+            free(atual);
+            atual = anterior;
+        }
+
+        anterior = atual;
+        atual = atual->proximo_no;
+    }
+}
+
--- lista3/atividade8/main.c
+++ lista3/atividade8/main.c
@@ -2,26 +2,36 @@
 #include <stdlib.h>
 #include "lista.h"
 
-int main(int argc, char* argv[]){
-
+int main(int argc, char* argv[]) {
     No* n0 = no('C', NULL);
     No* n1 = no('E', NULL);
     No* n2 = no('A', no('D', no('B', no('X', no('Z', NULL)))));
     No* n3 = no('U', NULL);
+
     lista_inserir_no(n0, n2);
     lista_inserir_no(n0, n1);
     lista_inserir_no(n0, n3);
+
+    printf("Lista original: ");
     lista_imprimir(n0);
-    printf("\nqtd nos: %d", lista_quantidade_nos(n0));
+    printf("\nQuantidade de nós: %d\n", lista_quantidade_nos(n0));
+
     No* n_copia = lista_copiar(n0);
-    printf("\n");
+    printf("Lista cópia: ");
     lista_imprimir(n_copia);
-    lista_concatenar(n0, n_copia);
     printf("\n");
+
+    lista_concatenar(n0, n_copia);
+    printf("Lista após concatenação: ");
     lista_imprimir(n0);
+    printf("\n");
+
     lista_liberar(n0);
     n0 = NULL;
+
+    printf("Lista após liberar: ");
     lista_imprimir(n0);
+    printf("\n");
 
     exit(0);
 }
